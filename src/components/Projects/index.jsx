5b92cdcb76c4a74802b05a64b46fd9b11d257943
Ginjak <89803801+Ginjak@users.noreply.github.com>
2024-02-20 10:19:32 +0000

    Finished project page

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -10,51 +10,57 @@ function Projects() {
 
   return (
     <>
-      <div className="container-xxl py-3">
-        <div className="row row-cols-1 row-cols-md-2 g-4">
-          {projects.map((project) => {
-            return (
-              <div className="col project" key={project.id}>
-                <div className="card position-relative">
-                  <img
-                    src={project.image}
-                    className="card-img-top"
-                    alt="card-image"
-                  ></img>
-                  <div className="card-img-overlay d-flex flex-column justify-content-between">
-                    <h5 className="card-title text-center position-relative text-white mt-2 mt-lg-4">
-                      {project.title}
-                    </h5>
-                    <div className="info-wraper text-white pb-0 py-lg-3">
-                      <div className="languages d-flex flex-wrap my-2 my-lg-3">
-                        {project.languages.map((value, index) => (
-                          <div key={index} className="button-lang me-2 mt-1">
-                            {value}
-                          </div>
-                        ))}
-                      </div>
-                      <div className="links">
-                        <a
-                          className="card-link"
-                          href={"https://github.com/Ginjak/" + project.github}
-                          target="_blank"
-                        >
-                          <i className="fa-brands fa-github"></i>
-                        </a>
-                        <a
-                          className="card-link"
-                          href={project.link}
-                          target="_blank"
-                        >
-                          <i className="fa-solid fa-globe"></i>
-                        </a>
+      <div className="projects-wraper">
+        <div className="container-xxl pb-3 pb-lg-5 ">
+          <h1 className="portfolio-title py-3 text-white text-uppercase">
+            Recent project
+          </h1>
+          <div className="row row-cols-1 row-cols-md-2 g-4">
+            {projects.map((project) => {
+              return (
+                <div className="col project" key={project.id}>
+                  <div className="card position-relative">
+                    <img
+                      src={project.image}
+                      className="card-img-top"
+                      alt="card-image"
+                    ></img>
+                    <div className="card-img-overlay d-flex flex-column justify-content-between">
+                      <h5 className="card-title text-center position-relative text-white mt-2 mt-lg-4">
+                        {project.title}
+                      </h5>
+                      <div className="info-wraper text-white pb-0 py-lg-3">
+                        <h5 className="fs-6">Technologies</h5>
+                        <div className="languages d-flex flex-wrap my-2 my-lg-3">
+                          {project.languages.map((value, index) => (
+                            <div key={index} className="button-lang me-2 mt-1">
+                              {value}
+                            </div>
+                          ))}
+                        </div>
+                        <div className="links">
+                          <a
+                            className="card-link"
+                            href={"https://github.com/Ginjak/" + project.github}
+                            target="_blank"
+                          >
+                            <i className="fa-brands fa-github"></i>
+                          </a>
+                          <a
+                            className="card-link"
+                            href={project.link}
+                            target="_blank"
+                          >
+                            <i className="fa-solid fa-globe"></i>
+                          </a>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
       </div>
     </>
--- src/pages/Portfolio/index.jsx
+++ src/pages/Portfolio/index.jsx
@@ -6,7 +6,6 @@ const Portfolio = () => {
   return (
     <>
       <Header>
-        <h1 className="portfolio-title container-xxl py-3">Recent project</h1>
         <Projects />
       </Header>
       <Footer />
