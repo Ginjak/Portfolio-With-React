f3b646230eff509ee6667126ea23538099a25232
Ginjak <89803801+Ginjak@users.noreply.github.com>
2024-02-20 09:58:33 +0000

    Added more sytling to portfolio page and projecs component

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -10,8 +10,7 @@ function Projects() {
 
   return (
     <>
-      <h1>Portfolio page</h1>
-      <div className="container-xxl">
+      <div className="container-xxl py-3">
         <div className="row row-cols-1 row-cols-md-2 g-4">
           {projects.map((project) => {
             return (
@@ -23,26 +22,31 @@ function Projects() {
                     alt="card-image"
                   ></img>
                   <div className="card-img-overlay d-flex flex-column justify-content-between">
-                    <h5 className="card-title position-relative text-white">
+                    <h5 className="card-title text-center position-relative text-white mt-2 mt-lg-4">
                       {project.title}
                     </h5>
-                    <div className="info-wraper text-white">
-                      <div className="languages d-flex">
+                    <div className="info-wraper text-white pb-0 py-lg-3">
+                      <div className="languages d-flex flex-wrap my-2 my-lg-3">
                         {project.languages.map((value, index) => (
-                          <div key={index} className="button">
+                          <div key={index} className="button-lang me-2 mt-1">
                             {value}
                           </div>
                         ))}
                       </div>
                       <div className="links">
                         <a
+                          className="card-link"
                           href={"https://github.com/Ginjak/" + project.github}
                           target="_blank"
                         >
-                          <i class="fa-brands fa-github"></i>
+                          <i className="fa-brands fa-github"></i>
                         </a>
-                        <a href={project.link} target="_blank">
-                          <i class="fa-solid fa-globe"></i>
+                        <a
+                          className="card-link"
+                          href={project.link}
+                          target="_blank"
+                        >
+                          <i className="fa-solid fa-globe"></i>
                         </a>
                       </div>
                     </div>
--- src/pages/Portfolio/index.jsx
+++ src/pages/Portfolio/index.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "../../components/Header";
 import Projects from "../../components/Projects";
+import Footer from "../../components/Footer";
 const Portfolio = () => {
   return (
     <>
-      <p>Portfolio</p>
-      <Projects />
+      <Header>
+        <h1 className="portfolio-title container-xxl py-3">Recent project</h1>
+        <Projects />
+      </Header>
+      <Footer />
     </>
   );
 };
